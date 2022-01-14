24bd6fdab17b0e3213b590f53f855ef76e56e630
Mr_RLG <1309708049@qq.com>
2022-01-14 12:13:18 +0800

    dev提交代碼444444

--- meinian_common/src/test/java/TestGit.java
+++ meinian_common/src/test/java/TestGit.java
@@ -6,5 +6,7 @@ public class TestGit {
     public void test01(){
         System.out.println("11111111111111");
         System.out.println("22222222222222");
+        System.out.println("3333333333333333mastter");
+        System.out.println("4444444444444dev");
     }
 }
